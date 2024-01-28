f29b2b4b7dfff01a22787890b20e911ad5214471
MurabitoF <francomurabito@outlook.com>
2024-01-28 11:52:28 -0300

    app configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
 import { BoardModule } from "./board/board.module";
 import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-	imports: [AuthModule, BoardModule, PrismaModule],
+	imports: [
+		AuthModule,
+		BoardModule,
+		PrismaModule,
+		ConfigModule.forRoot({ isGlobal: true }),
+	],
 	controllers: [],
 	providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
+import { PrismaClientExceptionFilter } from "./prisma/prisma-client-exception.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const PORT = process.env.PORT || 8080;
+	const app = await NestFactory.create(AppModule);
+
+	app.setGlobalPrefix("api");
+
+	const config = new DocumentBuilder()
+		.setTitle("Task Board API")
+		.setDescription("Documentation for usage of the REST API")
+		.setVersion("1.0.0")
+		.addBearerAuth()
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("/api/docs", app, document);
+
+	const { httpAdapter } = app.get(HttpAdapterHost);
+	app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+	app.enableCors();
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+		}),
+	);
+
+	await app.listen(PORT);
 }
 bootstrap();
